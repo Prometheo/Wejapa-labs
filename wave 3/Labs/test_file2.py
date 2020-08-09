373f58966f2b753759177fe7a5008feabe7ef6d5
Prometheo <olaoyejnr@gmail.com>
2020-08-09 13:49:16 +0100

    wave 3

---
+++ wave 3/Labs/test_file2.py
@@ -0,0 +1,21 @@
+# '''
+# You decide you want to play a game where you are hiding 
+# a number from someone.  Store this number in a variable 
+# called 'answer'.  Another user provides a number called
+# 'guess'.  By comparing guess to answer, you inform the user
+# if their guess is too high or too low.
+
+# Fill in the conditionals below to inform the user about how
+# their guess compares to the answer.
+# '''
+answer = #provide answer
+guess = #provide guess
+
+if #provide conditional
+    result = "Oops!  Your guess was too low."
+elif #provide conditional
+    result = "Oops!  Your guess was too high."
+elif #provide conditional
+    result = "Nice!  Your guess matched the answer!"
+
+print(result)
---
+++ wave 3/Labs/zip_dict.py
@@ -0,0 +1,8 @@
+#Zip Lists to a Dictionary
+#Use zip to create a dictionary cast that uses names as keys and heights as values.
+
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
+
+cast = # replace with your code
+print(cast)
